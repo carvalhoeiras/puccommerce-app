8362a2e1887ab6640afed9e48e4cb3025efa4b6e
GABRIEL ALVES <carvalhoeiras@poli.ufrj.br>
2020-12-24 18:07:08 -0300

    CRUD of products at Cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
               <header className="row">
                   <div>
-                      <a className= "brand" href="/">Puc-Commerce</a>
+                    <Link className="brand" to="/">Puc-Commerce</Link>
                   </div>
                   <div>
-                      <a href="/cart">Cesta</a>
-                      <a href="/signin">Login</a>
+                    <Link to="/cart">
+                      Cesta
+                      {cartItems.length > 0 && (<span className="badge">{cartItems.length}</span>)}
+                    </Link>
+                    <Link to="/signin">Login</Link>
                   </div>
               </header>
               <main>
+                <Route path="/cart/:id?" component={CartScreen}></Route>
                 <Route path="/product/:id" component={ProductScreen}></Route>
                 <Route path="/" component={HomeScreen} exact></Route>
               </main>
